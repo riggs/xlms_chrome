48e8ed1b32ff2217f3fc1d8789fdce4f8991d9b6
Benjamin Riggs <deisum@gmail.com>
2016-07-28 18:45:04 -0500

    Fleshing out project skeleton.

---
+++ background.js
@@ -0,0 +1,22 @@
+/**
+ * Created by riggs on 7/27/16.
+ */
+
+// https://developer.chrome.com/apps/app_runtime#event-onLaunched
+chrome.app.runtime.onLaunched.addListener((launch_data) => {
+  // If app was launched from XLMS.
+  if (launch_data.id === "launch_exercise") {
+    // https://developer.chrome.com/apps/app_window#method-create
+    chrome.app.window.create("UI.html", {id: "UI_window"}, (main_window) => {
+      // Set `window.launch_url` in the created window.
+      main_window.contentWindow.launch_url = launch_data.url;
+    });
+  } else {
+    // App wasn't launched from XLMS.
+    // TODO: Figure out bounds
+    chrome.app.window.create("error.html", {id: "error"}, (error_window) => {
+      error_window.contentWindow.error_message = "App much be launched from XLMS.";
+      // TODO: Configure error buttons
+    })
+  }
+});
